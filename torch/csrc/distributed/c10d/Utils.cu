dbc6315197bc81054f3a0babd3b3e5f7e23be963
Shuqiang Zhang <sqzhang@meta.com>
2024-05-10 14:45:49 -0700

    [c10d] Add an option for NAN check on every collective Summary: The NAN CHECK is done through device side assert without copying needed from GPU to CPU Test Plan: Unit test for collectives that should experience run time error
    (sqzhang_1) [sqzhangdevgpu009.cln1 ~/pytorch (38f5143e)]$  python
    test/distributed/test_c10d_nccl.py ProcessGroupNCCLTest.test_nan_assert
    /home/sqzhang/pytorch/torch/csrc/distributed/c10d/Utils.cu:15:
    checkForNaN: block: [0,0,0], thread: [0,0,0] Assertion `!isnan(val)`
    failed.
    /home/sqzhang/pytorch/torch/csrc/distributed/c10d/Utils.cu:15:
    checkForNaN: block: [0,0,0], thread: [1,0,0] Assertion `!isnan(val)`
    failed.
    /home/sqzhang/pytorch/torch/csrc/distributed/c10d/Utils.cu:15:
    checkForNaN: block: [0,0,0], thread: [2,0,0] Assertion `!isnan(val)`
    failed.
    /home/sqzhang/pytorch/torch/csrc/distributed/c10d/Utils.cu:15:
    checkForNaN: block: [0,0,0], thread: [3,0,0] Assertion `!isnan(val)`
    failed.
    /home/sqzhang/pytorch/torch/csrc/distributed/c10d/Utils.cu:15:
    checkForNaN: block: [0,0,0], thread: [4,0,0] Assertion `!isnan(val)`
    failed.
    /home/sqzhang/pytorch/torch/csrc/distributed/c10d/Utils.cu:15:
    checkForNaN: block: [0,0,0], thread: [5,0,0] Assertion `!isnan(val)`
    failed.
    [rank0]:[E507 17:31:56.885473996 Utils.cu:30] CUDA error during
    checkForNan: device-side assert triggered
    
    /home/sqzhang/pytorch/torch/csrc/distributed/c10d/Utils.cu:15:
    checkForNaN: block: [0,0,0], thread: [0,0,0] Assertion `!isnan(val)`
    failed.
    /home/sqzhang/pytorch/torch/csrc/distributed/c10d/Utils.cu:15:
    checkForNaN: block: [0,0,0], thread: [1,0,0] Assertion `!isnan(val)`
    failed.
    /home/sqzhang/pytorch/torch/csrc/distributed/c10d/Utils.cu:15:
    checkForNaN: block: [0,0,0], thread: [2,0,0] Assertion `!isnan(val)`
    failed.
    /home/sqzhang/pytorch/torch/csrc/distributed/c10d/Utils.cu:15:
    checkForNaN: block: [0,0,0], thread: [3,0,0] Assertion `!isnan(val)`
    failed.
    /home/sqzhang/pytorch/torch/csrc/distributed/c10d/Utils.cu:15:
    checkForNaN: block: [0,0,0], thread: [4,0,0] Assertion `!isnan(val)`
    failed.
    /home/sqzhang/pytorch/torch/csrc/distributed/c10d/Utils.cu:15:
    checkForNaN: block: [0,0,0], thread: [5,0,0] Assertion `!isnan(val)`
    failed.
    [rank1]:[E507 17:31:56.128961534 Utils.cu:30] CUDA error during
    checkForNan: device-side assert triggered
    
    .
    ----------------------------------------------------------------------
    Ran 1 test in 7.723s
    
    OK
    
    Tags:
    
    ghstack-source-id: 51cd8694b59b2f012429472871b06cccef39f21c
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/125726

---
+++ torch/csrc/distributed/c10d/Utils.cu
@@ -0,0 +1,42 @@
+#include <ATen/Dispatch.h>
+#include <ATen/cuda/CUDAContext.h>
+#include <c10/cuda/CUDAGuard.h>
+#include <torch/csrc/distributed/c10d/Utils.hpp>
+#include <torch/torch.h>
+#include <algorithm>
+
+namespace c10d {
+
+template <typename T>
+__global__ void checkForNaN(T* data, int size) {
+  int tid = blockIdx.x * blockDim.x + threadIdx.x;
+  int stride = blockDim.x * gridDim.x;
+
+  for (int i = tid; i < size; i += stride) {
+    CUDA_KERNEL_ASSERT(!isnan(data[i]));
+  }
+}
+
+void checkForNan(const at::Tensor& tensor) {
+  // skip check for non float types
+  if (!torch::is_floating_point(tensor)) {
+    return;
+  }
+  const int64_t maxNumThreadsPerBlock = 512;
+  const int64_t maxNumBlocks = 24;
+  const int numThreadsPerBlock =
+      std::min(maxNumThreadsPerBlock, tensor.numel());
+
+  const int numBlocks = std::min(
+      maxNumBlocks,
+      (tensor.numel() + numThreadsPerBlock - 1) / numThreadsPerBlock);
+
+  AT_DISPATCH_FLOATING_TYPES_AND_HALF(tensor.scalar_type(), "checkForNaN", [&] {
+    checkForNaN<scalar_t><<<numBlocks, numThreadsPerBlock>>>(
+        tensor.data_ptr<scalar_t>(), tensor.numel());
+    C10_CUDA_KERNEL_LAUNCH_CHECK();
+  });
+
+}
+
+} // namespace c10d
